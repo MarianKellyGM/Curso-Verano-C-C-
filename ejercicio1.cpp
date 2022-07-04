d2f67e9adc5d579a96295c2240c649f86d227624
MarianKellyGM <108492184+MarianKellyGM@users.noreply.github.com>
2022-07-04 15:38:38 -0500

    Add files via upload

---
+++ ejercicio1.cpp
@@ -0,0 +1,20 @@
+//ejercicio
+
+#include<stdio.h>
+
+int main(){
+	//idea de itzel
+	char nombre[10]={"marian"};
+	
+	printf("Nombre: %c",nombre[0]);
+	printf("%c",nombre[1]);
+	printf("%c",nombre[2]);
+	printf("%c",nombre[3]);
+	/*char nombre[15]; 
+	
+	printf("Ingresa tu nombre de pila: ");
+	scanf("%s",&nombre);
+	printf("Nombre: %s",nombre);
+	*/
+	return 0;
+}
